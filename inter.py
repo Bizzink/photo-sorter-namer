3d8eda2bdec01af9f884ec8c13be9c6666ae32c4
Biz <58650321+Bizzink@users.noreply.github.com>
2020-02-22 22:52:27 +1300

    code cleanup a bit

--- inter.py
+++ inter.py
@@ -3,45 +3,47 @@ from tkinter import ttk
 
 
 class Inter:
-    def __init__(self, toggle_png, toggle_jpg, copy_imgs):
+    def __init__(self, get_imgs, copy_imgs):
         self.root = Tk()
         self.root.title("Photo sorter namer")
         self.root.columnconfigure(0, weight=1)
-        self.root.columnconfigure(2, weight=1)
         self.root.rowconfigure(0, weight=1)
-
-        main_frame = ttk.Frame(self.root)
-        main_frame.grid(row=0, column=0, sticky=(N, W, E, S))
-        main_frame.columnconfigure(0, weight=1)
-        main_frame.rowconfigure(0, weight=1)
 
         self.source_path = StringVar()
         self.dest_path = StringVar()
         self.img_count = StringVar()
-        self.img_count.set("Image types: 0 found.")
         self.date_format = StringVar()
         self.date_format.set('%d-%m-%Y_%H-%M-%S')
-        self.png = BooleanVar()
-        self.png.set(False)
-        self.jpg = BooleanVar()
-        self.jpg.set(False)
+        self.img_types = StringVar()
+        self.img_types.set("png, jpg")
 
-        self.add_entry("Source path: ", 0, self.source_path, main_frame)
-        self.add_entry("Date format: ", 3, self.date_format, main_frame)
-        ttk.Separator(main_frame, orient="horizontal").grid(column=0, row=4)
-        self.add_entry("Destination path", 5, self.dest_path, main_frame)
-        ttk.Button(main_frame, text="Confirm", command=copy_imgs).grid(column=0, row=6, sticky=(W, E, S))
-        ttk.Separator(self.root, orient="vertical").grid(column=1, row=0)
+        main_frame = ttk.Frame(self.root)
+        main_frame.grid(row=0, column=0, sticky=(N, S, W, E))
+        main_frame.columnconfigure(0, weight=1)
+        main_frame.columnconfigure(2, weight=1)
+        main_frame.rowconfigure(0, weight=1)
 
-        # Img Types
-        types_frame = ttk.Frame(main_frame)
-        types_frame.grid(column=0, row=1, sticky=(W, E, N))
-        ttk.Label(types_frame, textvariable=self.img_count).grid(column=0, row=0, sticky=W)
-        ttk.Checkbutton(types_frame, text="PNG", variable=self.png, command=toggle_png).grid(column=0, row=1, sticky=W)
-        ttk.Checkbutton(types_frame, text="JPG", variable=self.jpg, command=toggle_jpg).grid(column=1, row=1, sticky=W)
+        controls_frame = ttk.Frame(main_frame)
+        controls_frame.grid(row=0, column=0, sticky=(W, E))
+        controls_frame.columnconfigure(0, weight=1)
+        controls_frame.rowconfigure(0, weight=1)
+
+        self.add_entry("Source path: ", 0, self.source_path, controls_frame)
+        self.add_entry("File types: ", 1, self.img_types, controls_frame, vcmd=get_imgs)
+
+        type_frame = ttk.Frame(controls_frame)
+        type_frame.grid(column=0, row=2, sticky=(W, E))
+        ttk.Button(type_frame, text="Check", command=get_imgs).grid(column=0, row=0, sticky=(W, E))
+        ttk.Label(type_frame, textvariable=self.img_count).grid(column=1, row=0, sticky=(W, E))
+        ttk.Label(controls_frame, text="").grid(column=0, row=3)
+        self.add_entry("Date format: ", 4, self.date_format, controls_frame)
+        self.add_entry("Destination path", 5, self.dest_path, controls_frame)
+        ttk.Button(controls_frame, text="Confirm", command=copy_imgs).grid(column=0, row=6, sticky=(W, S))
+
+        ttk.Separator(main_frame, orient="vertical").grid(column=1, row=0)
 
         # prefixes
-        prefix_main_frame = ttk.Frame(self.root)
+        prefix_main_frame = ttk.Frame(main_frame)
         prefix_main_frame.grid(column=2, row=0, sticky=(N, W, E, S))
         prefix_main_frame.columnconfigure(0, weight=1)
         ttk.Button(prefix_main_frame, text="Add Prefix", command=self.add_range).grid(column=0, row=0, sticky=(W, E, N))
@@ -50,31 +52,47 @@ class Inter:
         self.prefix_frame.columnconfigure(0, weight=1)
         self.prefix_frame.columnconfigure(1, weight=1)
         self.prefix_frame.columnconfigure(2, weight=1)
-        ttk.Label(self.prefix_frame, text="start date").grid(column=0, row=0, sticky=(W, E, S))
+        self.prefix_frame.columnconfigure(3, weight=1)
+        self.prefix_frame.columnconfigure(4, weight=1)
+        ttk.Label(self.prefix_frame, text="start date             ").grid(column=0, row=0, sticky=(W, E, S))
         ttk.Separator(self.prefix_frame, orient="vertical").grid(column=1, row=0, sticky=(W, E))
-        ttk.Label(self.prefix_frame, text="end date").grid(column=2, row=0, sticky=(W, E, S))
+        ttk.Label(self.prefix_frame, text="end date             ").grid(column=2, row=0, sticky=(W, E, S))
         ttk.Separator(self.prefix_frame, orient="vertical").grid(column=3, row=0, sticky=(W, E))
-        ttk.Label(self.prefix_frame, text="name").grid(column=4, row=0, sticky=(W, E, S))
+        ttk.Label(self.prefix_frame, text="name             ").grid(column=4, row=0, sticky=(W, E, S))
+
+        self.message = StringVar()
+        ttk.Label(self.root, textvariable=self.message).grid(row=1, column=0, sticky=(N, W, E))
 
         self.curr_range = 1
         self.ranges = []
 
-    def add_entry(self, label, row, variable, main_frame):
-        frame = ttk.Frame(main_frame)
+        self.root.update()
+        w, h = self.root.winfo_width(), self.root.winfo_height()
+        self.root.minsize(w, h)
+
+    def add_entry(self, label, row, variable, controls_frame, vcmd=None):
+        """add title /  entry pair"""
+        frame = ttk.Frame(controls_frame)
         frame.grid(column=0, row=row, sticky=(W, E, S))
         frame.columnconfigure(1, weight=1)
 
         ttk.Label(frame, text=label).grid(column=0, row=0, sticky=W)
 
-        entry = ttk.Entry(frame, width=30, textvariable=variable)
+        # Add command to run on each update if vcmd is not none
+        if vcmd is None:
+            entry = ttk.Entry(frame, width=20, textvariable=variable)
+        else:
+            entry = ttk.Entry(frame, width=20, textvariable=variable, validatecommand=vcmd, validate="focusout")
+
         entry.grid(column=1, row=0, sticky=(W, E))
 
     def add_range(self):
+        """add new range input"""
         date_range = {"start": StringVar(), "end": StringVar(), "name": StringVar()}
 
         if self.curr_range == 1:
             date_range["start"].set("1996-09-28")
-            date_range["end"].set("1997-02-13")
+            date_range["end"].set("25-07-2013")
             date_range["name"].set("Example")
 
         ttk.Entry(self.prefix_frame, width=10, textvariable=date_range["start"]).grid(column=0, row=self.curr_range, sticky=(W, E))
@@ -85,3 +103,27 @@ class Inter:
 
         self.ranges.append(date_range)
         self.curr_range += 1
+
+
+class ErrorMsg:
+    def __init__(self, message, message2 = None):
+        root = Tk()
+        root.title("Error!")
+        root.columnconfigure(0, weight=1)
+        root.rowconfigure(0, weight=1)
+
+        ttk.Label(root, text=message, anchor="center", pad=20).grid(row=0, column=0, sticky=(W, E))
+
+        if message2 is not None:
+            ttk.Label(root, text=message2, anchor="center", pad=20).grid(row=1, column=0, sticky=(W, E))
+            ttk.Button(root, text="close", command=root.destroy).grid(row=2, column=0, sticky=(W, E))
+
+        else:
+            ttk.Button(root, text="close", command=root.destroy).grid(row=1, column=0, sticky=(W, E))
+
+        root.update()
+        w, h = root.winfo_width(), root.winfo_height()
+        root.minsize(w, h)
+        root.resizable(0, 0)
+
+        root.mainloop()
--- photo_sort.py
+++ photo_sort.py
@@ -4,74 +4,96 @@ import shutil
 import datetime
 
 imgs = []
-types = []
 date_ranges = {}
 prev = None
 
 
-def toggle_png():
-    if ".png" in types:
-        types.remove(".png")
-    else:
-        types.append(".png")
-
-    get_imgs()
-
-
-def toggle_jpg():
-    if ".jpg" in types:
-        types.remove(".jpg")
-    else:
-        types.append(".jpg")
-
-    get_imgs()
-
-
 def get_imgs():
-    global types, imgs
+    """find all files in any subdirectory, if its filetype is in types, add it to imgs, set img_count to amount of files found"""
+    global imgs
     imgs = []
+
+    # convert types from string to list of file extensions
+    types = ui.img_types.get()
+    types = types.split(", ")
+
+    # add . at start of extension
+    for i in range(len(types)):
+        if not types[i][0] == ".":
+            types[i] = "." + types[i]
 
     for root, dirs, files in os.walk(ui.source_path.get()):
         for file in files:
+            # separate file name and extension
             name, ext = os.path.splitext(f"{root}\\{file}")
 
+            # if file is correct type
             if str(ext) in types:
-                # earlier of creation, modification time
+                # get earlier of creation time (ctime), modification time (mtime)
                 date = datetime.datetime.fromtimestamp(min(os.path.getmtime(f"{root}\\{file}"), os.path.getctime(f"{root}\\{file}")))
 
-                imgs.append({"path": f"{root}\\{file}", "name": file, "ext": ext,
-                             "date": date})
+                imgs.append({"path": f"{root}\\{file}", "name": file, "ext": ext, "date": date})
 
-    ui.img_count.set(f"Image types: {len(imgs)} found.")
+    ui.img_count.set(f" {len(imgs)} files found.")
 
 
 def str_to_datetime(string):
+    """convert date string (from date ranges entry) to datetime format"""
     global prev
-    string = string.get()
 
+    # preset vals
     if string == "today":
         return datetime.datetime.today()
 
     if string == "prev":
         return prev
 
-    year, month, day = string.split("-")
-    date = datetime.datetime(int(year), int(month), int(day))
+    # get year month, day depending on order they were entered
+    date = string.split("-")
+    if len(date[0]) == 4:
+        year, month, day = date
+
+    elif len(date[2]) == 4:
+        day, month, year = date
+
+    else:
+        raise ValueError("Invalid date format")
+
+    try:
+        date = datetime.datetime(int(year), int(month), int(day))
+    except:
+        raise ValueError("Invalid date format")
+
     return date
 
 
 def add_range(date_range):
+    """add a date range to ranges"""
     global prev
-    start = str_to_datetime(date_range["start"])
-    end = str_to_datetime(date_range["end"])
+
     name = date_range["name"].get()
+    start = date_range["start"].get()
+    end = date_range["end"].get()
+
+    try:
+        start = str_to_datetime(start)
+    except ValueError:
+        inter.ErrorMsg(f"Invalid start date format in range '{name}'! ({start})", message2=f"Format should be 'yyyy-mm-dd' or 'dd-mm-yyyy'")
+        return False
+
+    try:
+        end = str_to_datetime(end)
+    except ValueError:
+        inter.ErrorMsg(f"Invalid start date format in range '{name}'! ({end})", message2=f"Format should be 'yyyy-mm-dd' or 'dd-mm-yyyy'")
+        return False
 
     prev = end
-
     date_ranges[name] = {"start": start, "end": end}
+    return True
 
 
 def in_range(date):
+    """check if a date falls within a date range, if it does, return name of range"""
     for key in date_ranges.keys():
         if date_ranges[key]["start"] <= date <= date_ranges[key]["end"]:
             return str(key)
@@ -80,16 +102,24 @@ def in_range(date):
 
 
 def copy_imgs():
+    """copys all files gotten from get_imgs() to dest_path, renames them based on date format and range prefixes"""
+    for date_range in ui.ranges:
+        if not add_range(date_range):
+            return
+
     if not os.path.exists(ui.dest_path.get()):
         os.mkdir(ui.dest_path.get())
 
-    for date_range in ui.ranges:
-        add_range(date_range)
+    dest = ui.dest_path.get()
+
+    not_copied = 0
 
     for img in imgs:
-        dest = ui.dest_path.get()
-
-        shutil.copy(img["path"], dest)
+        try:
+            shutil.copy(img["path"], dest)
+        except shutil.SameFileError:
+            not_copied += 1
+            continue
 
         name = img["name"]
         ext = img["ext"]
@@ -105,6 +135,7 @@ def copy_imgs():
         while not success:
             try:
                 if dupe != 0:
+                    # add version number if duped name
                     os.rename(f"{dest}\\{name}", f"{dest}\\{new_name}_{dupe}{ext}")
                 else:
                     os.rename(f"{dest}\\{name}", f"{dest}\\{new_name}{ext}")
@@ -114,9 +145,21 @@ def copy_imgs():
                 success = False
                 dupe += 1
 
-        print(f"{dest}\\{name}".ljust(60, " ") + f"\t-->\t{dest}\\{date}{ext}")
+        if dupe != 0:
+            rename = f"{prefix}{date}_{dupe}{ext}"
+            print(f"{dest}\\{name}".ljust(60, " ") + f"\t-->\t{dest}\\{prefix}{date}_{dupe}{ext}")
+        else:
+            rename = f"{prefix}{date}{ext}"
+            print(f"{dest}\\{name}".ljust(60, " ") + f"\t-->\t{dest}\\{prefix}{date}{ext}")
+
+        ui.message.set(f"{name} --> {rename}")
+        ui.root.update()
+
+    if not_copied > 0:
+        ui.message.set(f"{not_copied} files not copied (already exist in destination folder)")
+        ui.root.update()
 
 
 if __name__ == "__main__":
-    ui = inter.Inter(toggle_png, toggle_jpg, copy_imgs)
+    ui = inter.Inter(get_imgs, copy_imgs)
     ui.root.mainloop()
